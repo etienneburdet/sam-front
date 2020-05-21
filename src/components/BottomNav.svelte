7f5282adaafcb39fdf0a141fd6e5865fae3a72f4
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-21 21:26:30 +0200

    basic bottom nav

---
+++ src/components/BottomNav.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { UserIcon, CalendarIcon } from 'svelte-feather-icons'
+</script>
+
+<div class="nav">
+  <CalendarIcon size="1.5x" class="nav-icon nav-icon-selected"/>
+  <UserIcon size="1.5x" class="nav-icon"/>
+</div>
+
+<style lang="scss">
+  .nav {
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 3rem;
+    border-top: 1px solid darkgray;
+    background: white;
+  }
+
+  :global(.nav-icon) {
+    padding: 0.5rem;
+    margin: 0.5rem;
+  }
+
+  :global(.nav-icon-selected) {
+    color: rgb(146, 23, 77);
+    border-top: 2px solid rgb(146, 23, 77);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,9 @@
 <script>
 	import Calendar from '../components/Calendar.svelte'
 	import Trainings from '../components/Trainings.svelte'
+	import BottomNav from '../components/BottomNav.svelte'
 </script>
 
 <Calendar/>
 <Trainings/>
+<BottomNav/>
